c291b8e6507ffa50fdac7e095e5c4d5e525fc123
Markus Ressel <markus.ressel@berlin.de>
2018-06-01 23:59:52 +0200

    added fuel to rest client addded basic classes

--- app/src/main/java/de/markusressel/mkdocseditor/view/activity/MainActivity.kt
+++ app/src/main/java/de/markusressel/mkdocseditor/view/activity/MainActivity.kt
@@ -1,11 +1,22 @@
 package de.markusressel.mkdocseditor.view.activity
 
+import android.os.Bundle
 import de.markusressel.mkdocseditor.R
 import de.markusressel.mkdocseditor.view.activity.base.DaggerSupportActivityBase
+import de.markusressel.rest.MkDocsRestApiClient
 
 class MainActivity : DaggerSupportActivityBase() {
     override val style: Int
         get() = DEFAULT
     override val layoutRes: Int
         get() = R.layout.activity_main
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super
+                .onCreate(savedInstanceState)
+        val client = MkDocsRestApiClient()
+        
+    }
+
 }
