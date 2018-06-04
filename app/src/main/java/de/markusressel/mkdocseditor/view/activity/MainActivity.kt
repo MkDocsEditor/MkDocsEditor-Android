e6ad544d9825c8e5354a57bbf52e4f1f08f9941d
Markus Ressel <markus.ressel@berlin.de>
2018-06-04 16:35:30 +0200

    added todo

--- app/src/main/java/de/markusressel/mkdocseditor/view/activity/MainActivity.kt
+++ app/src/main/java/de/markusressel/mkdocseditor/view/activity/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.os.Bundle
 import de.markusressel.mkdocseditor.R
 import de.markusressel.mkdocseditor.view.activity.base.DaggerSupportActivityBase
+import de.markusressel.mkdocseditor.view.fragment.DocumentsFragment
 import de.markusressel.mkdocsrestclient.MkDocsRestClient
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.rxkotlin.subscribeBy
@@ -25,34 +26,42 @@ class MainActivity : DaggerSupportActivityBase() {
         get() = R.layout.activity_main
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        super
+                .onCreate(savedInstanceState)
 
-        restClient.setHostname("10.0.2.2:8080")
+        restClient
+                .setHostname("10.0.2.2:8080")
 
 
-        restClient.getItemTree()
+        restClient
+                .getItemTree()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribeBy(
-                        onSuccess = {
-                            textView.text = it.toString()
-                        },
-                        onError = {
-                            textView.text = it.message
-                        }
-                )
+                .subscribeBy(onSuccess = {
+                    textView
+                            .text = it
+                            .toString()
+                }, onError = {
+                    textView
+                            .text = it
+                            .message
+                })
 
-//        restClient.getDocument("9360119919153839349")
-//                .subscribeOn(Schedulers.io())
-//                .observeOn(AndroidSchedulers.mainThread())
-//                .subscribeBy(
-//                        onSuccess = {
-//                            textView.text = it.toString()
-//                        },
-//                        onError = {
-//                            textView.text = it.message
-//                        }
-//                )
+        // TODO: Implement navigation and show list fragment
+        var fragment = DocumentsFragment()
+
+
+        //        restClient.getDocument("9360119919153839349")
+        //                .subscribeOn(Schedulers.io())
+        //                .observeOn(AndroidSchedulers.mainThread())
+        //                .subscribeBy(
+        //                        onSuccess = {
+        //                            textView.text = it.toString()
+        //                        },
+        //                        onError = {
+        //                            textView.text = it.message
+        //                        }
+        //                )
     }
 
 }
