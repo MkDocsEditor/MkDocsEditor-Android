51cc1ec7b955e83e187828aa4313d21536f5e30e
Markus Ressel <mail@markusressel.de>
2022-11-28 17:40:44 +0100

    wip foldable support

--- app/src/main/java/de/markusressel/mkdocseditor/feature/main/ui/compose/MainScreen.kt
+++ app/src/main/java/de/markusressel/mkdocseditor/feature/main/ui/compose/MainScreen.kt
@@ -10,47 +10,90 @@ import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.rememberDrawerState
 import androidx.compose.material.rememberScaffoldState
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import de.markusressel.mkdocseditor.feature.main.ui.DevicePosture
+import de.markusressel.mkdocseditor.feature.theme.MkDocsEditorTheme
 import de.markusressel.mkdocseditor.ui.activity.MainViewModel
 import de.markusressel.mkdocseditor.ui.activity.UiEvent
 import de.markusressel.mkdocseditor.ui.activity.UiState
 
 @Composable
 internal fun MainScreen(
-    mainViewModel: MainViewModel
+    mainViewModel: MainViewModel,
+    windowSize: Any,
+    devicePosture: DevicePosture
 ) {
     val uiState by mainViewModel.uiState.collectAsState()
 
     MainScreenContent(
         uiState = uiState,
-        onUiEvent = mainViewModel::onUiEvent
+        onUiEvent = mainViewModel::onUiEvent,
+        windowSize = windowSize,
+        devicePosture = devicePosture,
     )
 }
 
-@Preview
+
+@Preview(showBackground = true)
 @Composable
-private fun MainScreenContentPreview() {
-    MainScreenContent(
-        uiState = UiState(),
-        onUiEvent = {}
-    )
+fun MainScreenPreview() {
+    MkDocsEditorTheme {
+        MainScreenContent(
+            uiState = UiState(),
+            onUiEvent = {},
+            windowSize = WindowWidthSizeClass.Compact,
+            devicePosture = DevicePosture.NormalPosture,
+        )
+    }
+}
+
+@Preview(showBackground = true, widthDp = 700)
+@Composable
+fun MainScreenPreviewTablet() {
+    MkDocsEditorTheme {
+        MainScreenContent(
+            uiState = UiState(),
+            onUiEvent = {},
+            windowSize = WindowWidthSizeClass.Medium,
+            devicePosture = DevicePosture.NormalPosture,
+        )
+    }
+}
+
+@Preview(showBackground = true, widthDp = 1000)
+@Composable
+fun MainScreenPreviewDesktop() {
+    MkDocsEditorTheme {
+        MainScreenContent(
+            uiState = UiState(),
+            onUiEvent = {},
+            windowSize = WindowWidthSizeClass.Expanded,
+            devicePosture = DevicePosture.NormalPosture,
+        )
+    }
 }
 
 @Composable
 private fun MainScreenContent(
     uiState: UiState,
     onUiEvent: (UiEvent) -> Unit,
+    windowSize: Any,
+    devicePosture: DevicePosture,
 ) {
     val scaffoldState = rememberScaffoldState(rememberDrawerState(DrawerValue.Closed))
 
     Scaffold(
         scaffoldState = scaffoldState,
         topBar = {
-            TopAppBar(title = { Text("Top App Bar") }, backgroundColor = MaterialTheme.colors.primary)
+            TopAppBar(
+                title = { Text("Top App Bar") },
+                backgroundColor = MaterialTheme.colors.primary
+            )
         },
         drawerContent = {
 
---
+++ app/src/main/java/de/markusressel/mkdocseditor/ui/activity/MainActivityCompose.kt
@@ -0,0 +1,102 @@
+package de.markusressel.mkdocseditor.ui.activity
+
+import android.Manifest
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.window.layout.FoldingFeature
+import androidx.window.layout.WindowInfoTracker
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.single.PermissionListener
+import dagger.hilt.android.AndroidEntryPoint
+import de.markusressel.mkdocseditor.BuildConfig
+import de.markusressel.mkdocseditor.feature.main.ui.DevicePosture
+import de.markusressel.mkdocseditor.feature.main.ui.compose.MainScreen
+import de.markusressel.mkdocseditor.feature.main.ui.isBookPosture
+import de.markusressel.mkdocseditor.feature.main.ui.isSeparating
+import de.markusressel.mkdocseditor.feature.theme.MkDocsEditorTheme
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+@AndroidEntryPoint
+class MainActivityCompose : ComponentActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
+
+    @OptIn(
+        ExperimentalMaterial3WindowSizeClassApi::class,
+        ExperimentalMaterial3WindowSizeClassApi::class
+    )
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+        /**
+         * Flow of [DevicePosture] that emits every time there's a change in the windowLayoutInfo
+         */
+        val devicePostureFlow = WindowInfoTracker.getOrCreate(this).windowLayoutInfo(this)
+            .flowWithLifecycle(this.lifecycle)
+            .map { layoutInfo ->
+                val foldingFeature =
+                    layoutInfo.displayFeatures
+                        .filterIsInstance<FoldingFeature>()
+                        .firstOrNull()
+                when {
+                    isBookPosture(foldingFeature) ->
+                        DevicePosture.BookPosture(foldingFeature.bounds)
+
+                    isSeparating(foldingFeature) ->
+                        DevicePosture.Separating(foldingFeature.bounds, foldingFeature.orientation)
+
+                    else -> DevicePosture.NormalPosture
+                }
+            }
+            .stateIn(
+                scope = lifecycleScope,
+                started = SharingStarted.Eagerly,
+                initialValue = DevicePosture.NormalPosture
+            )
+
+        setContent {
+            MkDocsEditorTheme {
+                val windowSize = calculateWindowSizeClass(this)
+                val devicePosture by devicePostureFlow.collectAsState()
+                MainScreen(
+                    mainViewModel = viewModel,
+                    windowSize = windowSize.widthSizeClass,
+                    devicePosture = devicePosture,
+                )
+            }
+        }
+    }
+
+    private fun requestPermission() {
+        if (BuildConfig.DEBUG) {
+            Dexter.withContext(this)
+                .withPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .withListener(object : PermissionListener {
+                    override fun onPermissionGranted(response: PermissionGrantedResponse?) {}
+                    override fun onPermissionRationaleShouldBeShown(
+                        permission: PermissionRequest?,
+                        token: PermissionToken?
+                    ) {
+                    }
+
+                    override fun onPermissionDenied(response: PermissionDeniedResponse?) {}
+                }).check()
+        }
+    }
+
+}
