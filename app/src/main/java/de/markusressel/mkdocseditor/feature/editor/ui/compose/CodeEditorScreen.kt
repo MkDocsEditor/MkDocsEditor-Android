8f83f6e9b077b8c869a78e6f744d7536f4eaae18
Markus Ressel <mail@markusressel.de>
2022-12-22 00:06:37 +0100

    code editor experiments

--- app/src/main/java/de/markusressel/mkdocseditor/feature/editor/ui/compose/CodeEditorScreen.kt
+++ app/src/main/java/de/markusressel/mkdocseditor/feature/editor/ui/compose/CodeEditorScreen.kt
@@ -7,12 +7,17 @@ import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Snackbar
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextRange
@@ -20,6 +25,7 @@ import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import de.markusressel.kodehighlighter.core.ui.KodeTextField
+import de.markusressel.kodehighlighter.core.ui.KodeTextFieldDefaults
 import de.markusressel.kodehighlighter.language.markdown.MarkdownRuleBook
 import de.markusressel.kodehighlighter.language.markdown.colorscheme.DarkBackgroundColorSchemeWithSpanStyle
 import de.markusressel.mkdocseditor.feature.editor.ui.CodeEditorViewModel
@@ -40,17 +46,34 @@ internal fun CodeEditorScreen(
 
     val editorUiState by codeEditorViewModel.uiState.collectAsState()
 
+    var tfv: TextFieldValue by remember {
+        mutableStateOf(
+            TextFieldValue(
+                annotatedString = editorUiState.text ?: AnnotatedString(""),
+                selection = editorUiState.selection ?: TextRange.Zero,
+            )
+        )
+    }
+
+    remember(editorUiState.text, editorUiState.selection) {
+        tfv = tfv.copy(
+            annotatedString = editorUiState.text ?: AnnotatedString(""),
+            selection = editorUiState.selection ?: TextRange.Zero,
+        )
+        derivedStateOf { true }
+    }
+
     Column(
         modifier = modifier,
     ) {
         CodeEditorLayout(
-            modifier = Modifier.fillMaxSize(),
-            text = TextFieldValue(
-                annotatedString = editorUiState.text ?: AnnotatedString(""),
-                selection = editorUiState.selection ?: TextRange.Zero,
-            ),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            text = tfv,
             onTextChanged = {
                 codeEditorViewModel.onUserTextInput(it.annotatedString, it.selection)
+                tfv = it
             },
             // readOnly = editorUiState.editModeActive.not()
         )
@@ -89,5 +112,8 @@ private fun CodeEditorLayout(
         languageRuleBook = MarkdownRuleBook(),
         colorScheme = DarkBackgroundColorSchemeWithSpanStyle(),
         onValueChange = onTextChanged,
+        colors = KodeTextFieldDefaults.textFieldColors(
+            textColor = MaterialTheme.colorScheme.onBackground
+        ),
     )
 }
