3ce49cca453c75d7d8dcf47f052bb6e302e8520d
Markus Ressel <mail@markusressel.de>
2022-12-21 22:55:57 +0100

    added item animations

--- app/src/main/java/de/markusressel/mkdocseditor/feature/browser/ui/compose/SectionPath.kt
+++ app/src/main/java/de/markusressel/mkdocseditor/feature/browser/ui/compose/SectionPath.kt
@@ -1,5 +1,6 @@
 package de.markusressel.mkdocseditor.feature.browser.ui.compose
 
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
@@ -15,8 +16,13 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import de.markusressel.mkdocseditor.feature.browser.ui.usecase.SectionItem
@@ -45,6 +51,10 @@ internal fun SectionPath(
     onSectionClicked: (SectionItem) -> Unit,
     modifier: Modifier = Modifier,
 ) {
+    var previousPathItems: Set<SectionItem> by remember {
+        mutableStateOf(emptySet())
+    }
+
     Column(
         modifier = modifier,
     ) {
@@ -53,9 +63,31 @@ internal fun SectionPath(
                 .background(color = MaterialTheme.colorScheme.primaryContainer)
                 .then(modifier),
         ) {
-            path.forEach { pathItem ->
+            (previousPathItems + path).forEach { pathItem ->
+
+                val animatedAlpha by animateFloatAsState(
+                    targetValue = when {
+                        // item was added just now
+                        previousPathItems.contains(pathItem).not() && path.contains(pathItem) -> {
+                            previousPathItems = previousPathItems + pathItem
+                            0.1F
+                        }
+                        // item was added before and is "stable"
+                        previousPathItems.contains(pathItem) && path.contains(pathItem) -> 1F
+                        // item should be removed
+                        previousPathItems.contains(pathItem) && path.contains(pathItem).not() -> 0F
+                        else -> error("Unexpected case")
+                    },
+                    finishedListener = {
+                        if (it == 0F) {
+                            previousPathItems = previousPathItems.minus(pathItem)
+                        }
+                    }
+                )
+
                 Card(
                     modifier = Modifier
+                        .alpha(animatedAlpha)
                         .wrapContentWidth()
                         .defaultMinSize(minWidth = 48.dp) // for some reason its not possible to create a card thats less wide than 48.dp
                         .padding(2.dp),
