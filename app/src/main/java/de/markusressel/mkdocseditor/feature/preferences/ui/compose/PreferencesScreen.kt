6b177c2c0d63bdadc92b01d79f82eb3c63d2fce2
Markus Ressel <mail@markusressel.de>
2023-01-19 22:21:02 +0100

    remove padding

--- app/src/main/java/de/markusressel/mkdocseditor/feature/preferences/ui/compose/PreferencesScreen.kt
+++ app/src/main/java/de/markusressel/mkdocseditor/feature/preferences/ui/compose/PreferencesScreen.kt
@@ -3,12 +3,10 @@ package de.markusressel.mkdocseditor.feature.preferences.ui.compose
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import de.markusressel.kutepreferences.ui.theme.KutePreferencesTheme
 import de.markusressel.kutepreferences.ui.views.KuteOverview
@@ -35,9 +33,7 @@ internal fun PreferencesScreen(
             val currentItems by viewModel.currentPreferenceItems.collectAsState(initial = emptyList())
 
             KuteOverview(
-                modifier = Modifier
-                    .padding(8.dp)
-                    .fillMaxWidth(),
+                modifier = Modifier.fillMaxWidth(),
                 items = currentItems
             )
         }
