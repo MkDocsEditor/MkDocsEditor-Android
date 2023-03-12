e7b384172c1df03c80ee0c8491c4fa099c51aab0
Markus Ressel <mail@markusressel.de>
2023-03-12 23:57:45 +0100

    fix background color

--- app/src/main/java/de/markusressel/mkdocseditor/feature/about/ui/compose/AboutScreen.kt
+++ app/src/main/java/de/markusressel/mkdocseditor/feature/about/ui/compose/AboutScreen.kt
@@ -1,11 +1,11 @@
 package de.markusressel.mkdocseditor.feature.about.ui.compose
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.mikepenz.aboutlibraries.ui.compose.LibrariesContainer
 import com.mikepenz.aboutlibraries.ui.compose.LibraryDefaults
@@ -19,9 +19,7 @@ fun AboutScreen(
         header = {
             item {
                 AboutHeader(
-                    modifier = Modifier
-                        .background(MaterialTheme.colorScheme.surface)
-                        .padding(8.dp)
+                    modifier = Modifier.padding(8.dp)
                 )
             }
         },
@@ -29,7 +27,7 @@ fun AboutScreen(
         showVersion = true,
         showLicenseBadges = true,
         colors = LibraryDefaults.libraryColors(
-            backgroundColor = MaterialTheme.colorScheme.surface,
+            backgroundColor = Color.Transparent,
             contentColor = MaterialTheme.colorScheme.contentColorFor(MaterialTheme.colorScheme.background),
             badgeBackgroundColor = MaterialTheme.colorScheme.primary,
             badgeContentColor = MaterialTheme.colorScheme.contentColorFor(MaterialTheme.colorScheme.primary),
