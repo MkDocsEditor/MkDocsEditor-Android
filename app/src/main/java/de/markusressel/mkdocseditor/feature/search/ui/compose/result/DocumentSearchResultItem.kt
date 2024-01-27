80a3b98df433366dc1d387fadcb33bc2c8b39c94
Markus Ressel <mail@markusressel.de>
2024-01-27 23:56:52 +0100

    search improvements

--- app/src/main/java/de/markusressel/mkdocseditor/feature/search/ui/compose/result/DocumentSearchResultItem.kt
+++ app/src/main/java/de/markusressel/mkdocseditor/feature/search/ui/compose/result/DocumentSearchResultItem.kt
@@ -4,11 +4,13 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import de.markusressel.mkdocseditor.feature.search.domain.SearchResultItem
@@ -26,7 +28,13 @@ internal fun DocumentSearchResultItem(
         ) {
             Text(text = item.documentName)
 
-            Box(modifier = Modifier.background(MaterialTheme.colorScheme.inverseOnSurface)) {
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .clip(RoundedCornerShape(8.dp))
+                    .background(MaterialTheme.colorScheme.inverseOnSurface)
+                    .padding(8.dp)
+            ) {
                 Text(text = item.documentExcerpt)
             }
         }
