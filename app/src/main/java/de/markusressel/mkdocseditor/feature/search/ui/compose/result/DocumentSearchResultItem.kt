541ffba2cf8949b46346bd168041f76450f3abca
Markus Ressel <mail@markusressel.de>
2024-01-31 00:46:33 +0100

    search improvements

--- app/src/main/java/de/markusressel/mkdocseditor/feature/search/ui/compose/result/DocumentSearchResultItem.kt
+++ app/src/main/java/de/markusressel/mkdocseditor/feature/search/ui/compose/result/DocumentSearchResultItem.kt
@@ -4,8 +4,12 @@ import androidx.annotation.ColorInt
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -13,11 +17,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.unit.dp
+import com.mikepenz.iconics.compose.Image
+import com.mikepenz.iconics.typeface.library.materialdesigniconic.MaterialDesignIconic
 import de.markusressel.kodehighlighter.core.LanguageRuleBook
 import de.markusressel.kodehighlighter.core.StyleFactory
 import de.markusressel.kodehighlighter.core.colorscheme.ColorScheme
@@ -27,6 +35,8 @@ import de.markusressel.kodehighlighter.core.rule.RuleMatch
 import de.markusressel.kodehighlighter.core.ui.KodeText
 import de.markusressel.kodehighlighter.language.markdown.MarkdownRuleBook
 import de.markusressel.kodehighlighter.language.markdown.colorscheme.DarkBackgroundColorSchemeWithSpanStyle
+import de.markusressel.mkdocseditor.R
+import de.markusressel.mkdocseditor.feature.common.ui.compose.colorAttribute
 import de.markusressel.mkdocseditor.feature.search.domain.SearchResultItem
 import de.markusressel.mkdocseditor.feature.search.ui.compose.SearchResultCard
 import de.markusressel.mkdocseditor.feature.theme.MkDocsEditorTheme
@@ -49,9 +59,21 @@ internal fun DocumentSearchResultItem(
         Column(
             modifier = Modifier.padding(8.dp)
         ) {
-            Text(text = item.documentName)
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    modifier = Modifier
+                        .size(16.dp)
+                        .padding(end = 4.dp),
+                    asset = MaterialDesignIconic.Icon.gmi_file,
+                    colorFilter = ColorFilter.tint(colorAttribute(R.attr.documentBackgroundColor)),
+                )
+                Text(text = item.documentName)
+            }
 
             if (item.documentExcerptData != null) {
+                Spacer(modifier = Modifier.height(4.dp))
                 Box(
                     modifier = Modifier
                         .fillMaxWidth()
--- app/src/main/java/de/markusressel/mkdocseditor/feature/search/ui/compose/result/ResourceSearchResultItem.kt
+++ app/src/main/java/de/markusressel/mkdocseditor/feature/search/ui/compose/result/ResourceSearchResultItem.kt
@@ -1,11 +1,19 @@
 package de.markusressel.mkdocseditor.feature.search.ui.compose.result
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.unit.dp
+import com.mikepenz.iconics.compose.Image
+import com.mikepenz.iconics.typeface.library.materialdesigniconic.MaterialDesignIconic
+import de.markusressel.mkdocseditor.R
+import de.markusressel.mkdocseditor.feature.common.ui.compose.colorAttribute
 import de.markusressel.mkdocseditor.feature.search.domain.SearchResultItem
 import de.markusressel.mkdocseditor.feature.search.ui.compose.SearchResultCard
 import de.markusressel.mkdocseditor.feature.theme.MkDocsEditorTheme
@@ -27,7 +35,16 @@ internal fun ResourceSearchResultItem(
         Column(
             modifier = Modifier.padding(8.dp)
         ) {
-            Text(text = item.resourceName)
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Image(
+                    modifier = Modifier
+                        .size(16.dp)
+                        .padding(end = 4.dp),
+                    asset = MaterialDesignIconic.Icon.gmi_attachment,
+                    colorFilter = ColorFilter.tint(colorAttribute(R.attr.resourceBackgroundColor)),
+                )
+                Text(text = item.resourceName)
+            }
         }
     }
 }
