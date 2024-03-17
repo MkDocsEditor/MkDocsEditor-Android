8a8d78ac20cd53b00918b493967805169a0aa12c
Markus Ressel <mail@markusressel.de>
2024-03-17 17:45:27 +0100

    remove unused code

--- app/src/main/java/de/markusressel/mkdocseditor/extensions/common/MimeTypeExtensions.kt
+++ app/src/main/java/de/markusressel/mkdocseditor/extensions/common/MimeTypeExtensions.kt
@@ -4,14 +4,6 @@ import android.content.ContentResolver
 import android.content.Context
 import android.net.Uri
 import android.webkit.MimeTypeMap
-import androidx.core.net.toUri
-import java.io.File
-import java.nio.file.Path
-import kotlin.io.path.absolutePathString
-
-fun File.getMimeType(context: Context): String = toPath().getMimeType(context)
-
-fun Path.getMimeType(context: Context): String = absolutePathString().toUri().getMimeType(context)
 
 fun Uri.getMimeType(context: Context): String {
     return if (ContentResolver.SCHEME_CONTENT == scheme) {
@@ -21,13 +13,4 @@ fun Uri.getMimeType(context: Context): String {
         val fileExtension = MimeTypeMap.getFileExtensionFromUrl(toString())
         MimeTypeMap.getSingleton().getMimeTypeFromExtension(fileExtension.lowercase())
     } ?: "*/*"
-}
-
-fun String.getMimeType(): String {
-    val extension = MimeTypeMap.getFileExtensionFromUrl(this)
-    val type = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension)
-    return when {
-        type.isNullOrBlank() -> "*/*"
-        else -> type
-    }
 }
